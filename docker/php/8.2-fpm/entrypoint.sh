a0c4c17640ae3b1f7d515f131763471103bac690
Jorge Luiz <jorgeluiz1586@gmail.com>
2023-09-09 12:53:56 -0300

    feat: add s3 upload

--- docker/php/8.2-fpm/entrypoint.sh
+++ docker/php/8.2-fpm/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-php artisan server --host=0.0.0.0
+php artisan serve --host=0.0.0.0
 #php-fpm -D
 ##nginx -g "daemon off;"
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('image/upload', function (Request $request) {
+    try {
+        $request->file->storeAs('example', $request->file->getClientOriginalName(), 's3');
+        return response(["message" => "Arquivo armazenado com sucesso"], 200);
+    } catch (\Exception $e) {
+        throw new \Exception($e->getMessage());
+    }
 });
